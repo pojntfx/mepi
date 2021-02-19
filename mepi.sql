e2b5000e5f2ffb4cd938078d379cc5d7f4366f3f
Felix Pojtinger <felix@pojtinger.com>
2021-02-19 17:14:22 +0100

    feat: Add constraints for customers

--- mepi.sql
+++ mepi.sql
@@ -1,8 +1,17 @@
 -- Disable errors
 whenever sqlerror continue;
 
--- Drop old constraints
+-- Drop old other constraints
 alter table contact_address drop constraint region_name_must_be_known;
+alter table customer drop constraint street_credit_must_be_in_range;
+
+-- Drop old foreign key constraints
+alter table customer drop constraint customer_address_fk;
+
+-- Drop primary key constraints
+alter table contact_address drop constraint contact_address_pk;
+alter table customer drop constraint customer_pk;
+
 -- Drop old tables
 drop table contact_address;
 drop table customer;
@@ -28,15 +37,13 @@ create table contact_address (
     house_name varchar2(80) not null
 );
 create table customer (
-    customer_id number not null,
     first_name varchar2(80) not null,
     last_name varchar2(80),
-    birthday date not null,
+    birthday date,
     street_credit number,
-    address_id number
+    contact_address_id number
 );
 create table contract (
-    contract_id number not null,
     acceptance_date date not null,
     duration interval year to month not null,
     customer_id number,
@@ -46,7 +53,6 @@ create table contract (
     property_id number
 );
 create table plan (
-    plan_id number not null,
     name varchar(80) not null,
     base_monthly_cost float(2) not null,
     warning_interval interval year to month not null,
@@ -55,26 +61,21 @@ create table plan (
     initial_cost float(2) not null
 );
 create table property (
-    property_id number not null,
     product_id number,
     contract_id number
 );
 create table product (
-    product_id number not null,
     name varchar2(80),
     description varchar2(80)
 );
 create table bill (
-    bill_id number not null,
     contract_id number
 );
 create table payment (
-    payment_id number not null,
     bill_id number,
     payment_date date
 );
 create table claim (
-    claim_id number not null,
     compensation_amount float(2),
     rejected number not null,
     rejected_reason varchar2(255),
@@ -82,19 +83,16 @@ create table claim (
     claim_date date
 );
 create table payout (
-    payout_id number not null,
     claim_id number,
     payout_date date
 );
 create table payment_method (
-    priority_id integer not null,
     payment_method_id integer,
     customer_id integer,
     external_id integer,
     ledger varchar2(80)
 );
 create table risk (
-    risk_id integer not null,
     reason varchar2(80),
     multiplier float
 );
@@ -102,7 +100,21 @@ create table risk (
 -- Create identity columns
 alter table contact_address
 add contact_address_id number generated always as identity;
+alter table customer
+add customer_id number generated always as identity;
 
--- Create new constraints
+-- Create new primary key constraints
+alter table contact_address
+add constraint contact_address_pk primary key(contact_address_id);
+alter table customer
+add constraint customer_pk primary key(customer_id);
+
+-- Create new foreign key constraints
+alter table customer
+add constraint customer_address_fk foreign key(contact_address_id) references contact_address(contact_address_id);
+
+-- Create new other constraints
 alter table contact_address
 add constraint region_name_must_be_known check(region_name in ('eria', 'rhov', 'gond', 'mord'));
+alter table customer
+add constraint street_credit_must_be_in_range check(street_credit between 1 and 10);
